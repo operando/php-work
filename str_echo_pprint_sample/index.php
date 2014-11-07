ea67c916de42e67f11e803843009418846c94304
Shinobu Okano Mac <okanoshinobu8@gmail.com>
2014-11-07 22:01:11 +0900

    Add str_echo_pprint_sample

---
+++ str_echo_pprint_sample/index.php
@@ -0,0 +1,20 @@
+<?php
+
+$result = print "Hello World!\n";
+print $result;
+
+
+echo "test",23,"test","\n"; // OK
+// print "test",23,"test","\n"; // NG
+
+// $echo_function = 'echo';
+// $echo_function("test"); // NG
+
+// $print_function = 'print';
+// $print_function("test"); // NG
+
+$test_double = 'double';
+echo "test $test_double\n";
+
+$test_single = 'single';
+echo 'test $test_single\n';
