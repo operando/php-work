1abfb70afcd21bf8a0369247bbe9b6ded168f3c8
Shinobu Okano Mac <okanoshinobu8@gmail.com>
2014-11-10 20:48:11 +0900

    Update closure_sample

--- closure_sample/index.php
+++ closure_sample/index.php
@@ -5,3 +5,36 @@ $greet = function($name) {
 };
 
 $greet("PHP");
+
+
+function counter() {
+    $count = 0;
+    return function() use (&$count) {
+        return ++$count;
+    };
+}
+
+$counter = counter();
+echo $counter() . PHP_EOL;
+echo $counter() . PHP_EOL;
+echo $counter() . PHP_EOL;
+$count = 0;
+echo $count . PHP_EOL;
+echo $counter() . PHP_EOL;
+
+class User {
+    private $id;
+    
+    function __construct($id) {
+        $this->id = $id;
+    }
+
+    public function getId() {
+        return function() { return $this->id; };
+    }
+}
+
+$user = new User(5);
+$getUserId = $user->getId();
+echo $getUserId() . PHP_EOL;
+
